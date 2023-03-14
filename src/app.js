2a165b511dc6cc85191e596f13f50d980e79569f
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2023-03-14 04:26:49 +0000

    Excuse Generator Finalizado

--- src/app.js
+++ src/app.js
@@ -8,4 +8,26 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let excuse = `${fraseRandom(who)} ${fraseRandom(action)} ${fraseRandom(
+    what
+  )} ${fraseRandom(when)}`;
+  let element = document.getElementById("excuse");
+  element.innerHTML = excuse;
 };
+
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function fraseRandom(array) {
+  let pick = array[Math.floor(Math.random() * array.length)];
+  return pick;
+}
